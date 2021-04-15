10020b86f6f456a5f6401102319733e359d82987
gimminseog <gm27376@gmail.com>
2021-04-15 16:37:41 +0900

    Learn binding & use data

--- learn-vue/src/App.vue
+++ learn-vue/src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name:"App",
+        data(){
+            return {
+                name: "",
+                age: "",
+                users:[
+                    {
+                        name: "minseok",
+                        age: 28
+                    }
+                ]
+            }
+        },
+        methods:{
+            addUser(){
+                this.users.push({
+                    name: this.name,
+                    age: 28
+                });
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
